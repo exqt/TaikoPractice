df07532dc04feb971f68afce4cae75f81155674b
exqt <8168124+exqt@users.noreply.github.com>
2025-07-05 00:52:05 +0900

    feat: add additional drum key input

--- Assets/Scripts/Common/Threads/InputThread.cs
+++ Assets/Scripts/Common/Threads/InputThread.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using UnityEngine;
 using UnityEngine.Events;
@@ -10,12 +11,12 @@ public class InputThread : ThreadBase
     private static extern short GetAsyncKeyState(int vKey);
     #endregion
 
-    readonly TaikoKeyType[] keysToRead = new TaikoKeyType[]
+    readonly Dictionary<TaikoKeyType, char[]> keyToChars = new Dictionary<TaikoKeyType, char[]>()
     {
-        TaikoKeyType.LEFT_KA,
-        TaikoKeyType.LEFT_DON,
-        TaikoKeyType.RIGHT_DON,
-        TaikoKeyType.RIGHT_KA
+        { TaikoKeyType.LEFT_KA, new char[] { 'E', 'D' } },
+        { TaikoKeyType.LEFT_DON, new char[] { 'R', 'F' } },
+        { TaikoKeyType.RIGHT_DON, new char[] { 'I', 'J' } },
+        { TaikoKeyType.RIGHT_KA, new char[] { 'O', 'K' } }
     };
 
     readonly bool[] state = new bool[256];
@@ -33,21 +34,23 @@ public class InputThread : ThreadBase
     // 0x0001 이전에 누른 적이 있고 호출 시점에는 눌려있지 않은 상태
     // 0x8000 이전에 누른 적이 없고 호출 시점에는 눌려있는 상태
     // 0x8001 이전에 누른 적이 있고 호출 시점에도 눌려있는 상태
-
     protected override void Update()
     {
-        for (int i = 0; i < keysToRead.Length; i++)
+        foreach (var pair in keyToChars)
         {
-            var key = (int)keysToRead[i];
-            var prevState = state[key];
-            var currState = (GetAsyncKeyState(key) & 0x8000) != 0;
-
-            if (!prevState && currState)
+            var key = (int)pair.Key;
+            foreach (var c in pair.Value)
             {
-                OnKeyPressed?.Invoke(keysToRead[i]);
-            }
+                var prevState = state[c];
+                var currState = (GetAsyncKeyState(c) & 0x8000) != 0;
 
-            state[key] = currState;
+                if (!prevState && currState)
+                {
+                    OnKeyPressed?.Invoke(pair.Key);
+                }
+
+                state[c] = currState;
+            }
         }
 
         OnUpdate?.Invoke();
